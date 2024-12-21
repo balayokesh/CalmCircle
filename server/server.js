34219762bf2b0095f18803ea2f82a0b6db5ed608
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2024-12-21 18:12:51 +0000

    added backend tempalte code

---
+++ server/server.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Configure routes
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
+
+// Connect database
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+app.listen(PORT, () => {
+  console.log(`Server running at PORT: ${PORT}`);
+});
