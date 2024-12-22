2878994376bae75302bc8b0e37b514cbc53b63f9
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2024-12-22 08:18:57 +0000

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: LandingPageComponent },
+  { path: 'signup', component: SignUpComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
