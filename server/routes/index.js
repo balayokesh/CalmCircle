555c92e381e068f4a63c08a59437abf56de2618d
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2024-12-22 07:59:16 +0000

    added backend route for signup new user

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
 
-router.get("/", () => {
-  console.log("Called index route");
+// Import authenticator
+const checkLogin = require("../controllers/authenticate.js");
+
+// Import Models
+const User = require("../models/user.model");
+
+// Sign up
+router.post("/", (req, res) => {
+  let username = req.body.username;
+  let passwordHash = req.body.passwordHash;
+  let email = req.body.email;
+  let profilePicture = req.body.profilePicture;
+  let isAnonymous = req.body.isAnonymous;
+  const newUser = new User({
+    username,
+    passwordHash,
+    email,
+    profilePicture,
+    isAnonymous,
+  });
+  newUser
+    .save()
+    .then(() => res.json(`User ${username} created successfully`))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 module.exports = router;
