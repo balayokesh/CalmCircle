0e5d5397f949a35aa5c5c3e0fcd380f93539edaa
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2024-12-23 18:17:04 +0000

    added ng-bootstrap

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgbNavModule, NgbNavModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   constructor(private authService: AuthService) {}
-
+  active = 'top';
   logout() {
     console.log('clicked log out');
     this.authService.logout();
