732b7bf3a1ba45c069ce95b91d30bcafa153e7eb
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2024-12-24 07:09:56 +0000

    ui enhanced with horizontal nav tabs in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { MoodLogComponent } from '../components/mood-log/mood-log.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NgbNavModule, NgbNavModule, MoodLogComponent],
+  imports: [NgbNavModule, NgbAlertModule, MoodLogComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  constructor(private authService: AuthService) {}
-  active = 'mood';
+  constructor(private authService: AuthService, private router: Router) {}
+  active = 1;
   logout() {
     console.log('clicked log out');
     this.authService.logout();
+  }
+
+  redirectToHome() {
+    this.router.navigate(['/']);
   }
 }
