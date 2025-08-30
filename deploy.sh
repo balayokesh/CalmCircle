de73823453ed8fd0ea7b7743fe092b00f2a0cbf4
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2025-08-30 14:01:26 +0000

    updated deploy.sh with corrected deploy command

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 cd "$(dirname "$0")"
 
 # Build the Angular project
-ng build --configuration production --base-href https://balayokesh.github.io/CalmCircle/
+ng build --configuration production --base-href "/CalmCircle/"
 
 # Deploy to GitHub Pages
 npx angular-cli-ghpages --dir=dist/sandbox/browser
